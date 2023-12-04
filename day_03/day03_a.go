3721a8c7a9b5ba3b3843db8bee96c45950830a2a
Brennan Cropper <bcropper@gmail.com>
2023-12-04 15:57:38 -0600

    day 3 setup

---
+++ day_03/day03_a.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//Read input file
+	input, _ := os.Open("input.txt")
+	defer input.Close()
+	sc := bufio.NewScanner(input)
+
+	max_red := 12
+	max_green := 13
+	max_blue := 14
+
+	game_count := 0
+	for sc.Scan() {
+		line := sc.Text()
+		game := strings.Split(line, ":")
+		game_strid := strings.Split(game[0], " ")
+		game_num, _ := strconv.Atoi(game_strid[1])
+		game_data := strings.Split(game[1], ";")
+
+		current_red_max := 0
+		current_green_max := 0
+		current_blue_max := 0
+		for _, game_x := range game_data {
+			colors := strings.Split(game_x, ",")
+			re, _ := regexp.Compile("(\\d+)\\s(\\w+)")
+			count := 0
+			color_name := ""
+			for _, color := range colors {
+				results := re.FindAllStringSubmatch(color, -1)
+				count, _ = strconv.Atoi(results[0][1])
+				color_name = results[0][2]
+				switch {
+				case color_name == "red":
+					if count > current_red_max {
+						current_red_max = count
+					}
+				case color_name == "green":
+					if count > current_green_max {
+						current_green_max = count
+					}
+				case color_name == "blue":
+					if count > current_blue_max {
+						current_blue_max = count
+					}
+				}
+			}
+		}
+		if (current_red_max <= max_red) && (current_green_max <= max_green) && (current_blue_max <= max_blue) {
+			fmt.Println(line)
+			fmt.Printf("Adding game %d\n", game_num)
+			game_count += game_num
+		}
+	}
+
+	fmt.Println(game_count)
+}
